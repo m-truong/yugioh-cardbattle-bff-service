9a3461981bc6842d03429d8f9e19782006b7e9ca
Michael Truong <truong.michael.2013@gmail.com>
2023-06-11 15:10:02 -0400

    Update app.module.ts with nestjs autoSchemaFile
    Allows for code first approach

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), `src/schema.gql`),
     }),
   ],
   controllers: [AppController],
