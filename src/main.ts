41d25debfb10184288ccc645e968ad090a27d427
Michael Truong <truong.michael.2013@gmail.com>
2023-06-11 15:42:38 -0400

    Set up env file and cors and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
+import { LogLevel } from '@nestjs/common';
+// import ddTrace from 'dd-trace';
+
+// note: always just do 'npm install'
+//  in order to install required dependencies for any project
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  const LOG_LEVEL = (process.env.LOG_LEVEL as LogLevel) || 'error';
+
+  const app = await NestFactory.create(AppModule, {
+    logger: [LOG_LEVEL],
+  });
+
+  app.enableCors();
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
