0d7f83e87f9bf74915f9732aaff9e0cbaf8192f0
Michael Truong <truong.michael.2013@gmail.com>
2023-07-10 14:04:13 -0400

    Update monster-card DTO and controller service

--- src/monster-card/monster-card.controller.ts
+++ src/monster-card/monster-card.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { MonsterCardService } from './monster-card.service';
 import { CreateMonsterCardDto } from './dto/create-monster-card.dto';
 import { UpdateMonsterCardDto } from './dto/update-monster-card.dto';
+import { MonsterCard } from './entities/monster-card.entity';
 
-@Controller('monster-card')
+@Controller('monster-card') // path-prefix; groups related routes together
 export class MonsterCardController {
   constructor(private readonly monsterCardService: MonsterCardService) {}
 
-  @Post()
-  create(@Body() createMonsterCardDto: CreateMonsterCardDto) {
-    return this.monsterCardService.create(createMonsterCardDto);
-  }
-
   @Get()
-  findAll() {
+  findAll(@Req() request: Request): MonsterCard[] {
+    console.log('logging findAll() request', request);
     return this.monsterCardService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.monsterCardService.findOne(+id);
+  // route is '/monster-card/:name
+  @Get(':name') // dynamic argument?
+  findOneByName(@Param('name') name: string) {
+    return this.monsterCardService.findOne(+name);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMonsterCardDto: UpdateMonsterCardDto) {
-    return this.monsterCardService.update(+id, updateMonsterCardDto);
-  }
+  // @Post()
+  // create(@Body() createMonsterCardDto: CreateMonsterCardDto) {
+  //   return this.monsterCardService.create(createMonsterCardDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.monsterCardService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateMonsterCardDto: UpdateMonsterCardDto,
+  // ) {
+  //   return this.monsterCardService.update(+id, updateMonsterCardDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.monsterCardService.remove(+id);
+  // }
 }
--- src/monster-card/monster-card.service.ts
+++ src/monster-card/monster-card.service.ts
@@ -4,12 +4,17 @@ import { UpdateMonsterCardDto } from './dto/update-monster-card.dto';
 
 @Injectable()
 export class MonsterCardService {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
   create(createMonsterCardDto: CreateMonsterCardDto) {
     return 'This action adds a new monsterCard';
   }
 
   findAll() {
-    return `This action returns all monsterCard`;
+    console.log(`This action returns all monsterCard`);
+    // TODO: make an API fetch request to the yugioh pro-deck
+    // return this.axios
   }
 
   findOne(id: number) {
